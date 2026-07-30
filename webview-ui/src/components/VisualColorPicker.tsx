58acdd01b418fac8399b4e49f9464354919f3cb5
Florin Timbuc <florin@sowild.design>
2026-07-30 23:51:31 +0300

    fix: satisfy ESLint 10 assignment checks

--- webview-ui/src/components/VisualColorPicker.tsx
+++ webview-ui/src/components/VisualColorPicker.tsx
@@ -43,9 +43,9 @@ function hslToRgb(h: number, s: number, l: number): [number, number, number] {
   const c = (1 - Math.abs(2 * l - 1)) * s;
   const hp = h / 60;
   const x = c * (1 - Math.abs((hp % 2) - 1));
-  let r1 = 0;
-  let g1 = 0;
-  let b1 = 0;
+  let r1: number;
+  let g1: number;
+  let b1: number;
   if (hp < 1) [r1, g1, b1] = [c, x, 0];
   else if (hp < 2) [r1, g1, b1] = [x, c, 0];
   else if (hp < 3) [r1, g1, b1] = [0, c, x];
